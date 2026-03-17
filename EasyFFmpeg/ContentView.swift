e311f2a847505cb48d2caed9b8bed67e86620940
whyimhr <whyimhr>
2026-03-17 05:34:00 +0300

    Initial Commit

---
+++ EasyFFmpeg/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EasyFFmpeg
+//
+//  Created by Mikhail on 17.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EasyFFmpeg/EasyFFmpegApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyFFmpegApp.swift
+//  EasyFFmpeg
+//
+//  Created by Mikhail on 17.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct EasyFFmpegApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
